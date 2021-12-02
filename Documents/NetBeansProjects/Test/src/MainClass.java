062a96649b0c63223364b7204ecc3307b903d129
Whoislead <patrick.buck12@gmail.com>
2021-12-02 07:54:19 -0800

    commit of owner gui

---
+++ Documents/NetBeansProjects/Test/src/MainClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author patri
+ */
+public class MainClass {
+    public static void main(String[] args){
+        OrderorDeleteJFrame odmenu = new OrderorDeleteJFrame();
+        odmenu.show();
+        
+    }
+}
